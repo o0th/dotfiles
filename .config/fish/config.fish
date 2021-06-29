103c3a6192818bbd8edba5c69b836011cf3f8f3c
Sabato Luca Guadagno <o0th@pm.me>
2021-06-29 08:39:27 +0200

    export path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,11 @@
+# Disable welcome message
 set -U fish_greeting
+
+# Enable starship
 starship init fish | source
+
+fundle plugin 'FabioAntunes/fish-nvm'
+fundle plugin 'edc/bass'
+fundle init
+
+set -U fish_user_paths /home/o0th/.local/bin $fish_user_paths
